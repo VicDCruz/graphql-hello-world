9631af2c37b79d038da544ef778cdcfbf087ca9d
Victor Cruz <vicdan215@gmail.com>
2021-07-09 00:04:12 -0500

    Mutations: Creating Links

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { setContext } from '@apollo/client/link/context';
 
 import {
   ApolloProvider,
@@ -15,8 +16,18 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTYyNTc5NDIyMX0.1qLNm4Id2KyYdAngyeu4Cc95w9jGi1DjCT4FQwCbpDo',
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
