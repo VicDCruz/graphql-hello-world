54d3508bdcd19368ffd824e6dc6515ff2c2bc27f
Victor Cruz <vicdan215@gmail.com>
2021-07-08 21:17:44 -0500

    Queries: Loading links

---
+++ hackernews-react-apollo/src/components/Link.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Link({ link }) {
+    return (
+        <div>
+            <div>
+                {link.description} ({link.url})
+            </div>
+        </div>
+    )
+}
+
+export default Link
+
